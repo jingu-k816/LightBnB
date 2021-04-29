1729a1d13e356583049292e0990e28e0372c5ffd
Jingu Kang <jinguk0816@gmail.com>
2021-04-28 23:40:46 -0400

    implement getAllReservations function to work with the lightbnb database.

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,8 +1,9 @@
-SELECT properties.* reservations.*, AVG(property_reviews.rating) AS average_rating
-FROM properties
-JOIN property_reviews ON properties.id = property_id
-JOIN reservations ON reservations.property_id = properties.id
-WHERE reservations.guest_id = 1 AND reservations.end_date < now()::date
-GROUP BY properties.id, reservations.start_date
-ORDER BY start_date
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
 LIMIT 10;
--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -69,7 +69,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT properties.*, reservations.*, AVG(rating) AS average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`;
+  const queryParam = [guest_id, limit];
+
+  return pool
+  .query(queryString, queryParam)
+  .then(result => result.rows)
+  .catch(error => error.message);
 }
 exports.getAllReservations = getAllReservations;
 
